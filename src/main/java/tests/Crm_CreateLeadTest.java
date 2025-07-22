f6e8207fcd275ad5b02f56803dccf7de9409ed07
TeenaRaghav <teenaraghav3017@gmail.com>
2025-07-22 16:57:07 -0500

    Formatted the space in BaseTest and CreateLeadTest

--- src/main/java/tests/Crm_CreateLeadTest.java
+++ src/main/java/tests/Crm_CreateLeadTest.java
@@ -5,21 +5,19 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import listeners.ListenersCRM;
 
-
 import pages.CRM_LeadsPage;
 
 @Listeners(ListenersCRM.class)
-public class Crm_CreateLeadTest  extends Crm_BaseTest {
-	
-	
-	@Test
-    public void createLeadTest() throws InterruptedException {
-       WebDriver driver = getBrowser(); // getBrowser() method comes from Crm_BaseTest, 
-        //returning the current thread’s WebDriver instance — so you don’t need to initialize it here.
-      CRM_LeadsPage lp = hp.clickLeads();//navigates from hme page and clicks on the click leads method and driver is set to the lead page
-      lp.clickCreateLeadButtonAndGetTitle(); // Clicks on the Create Lead button and waits for the title to change
-     
- 
-    }
-}
+public class Crm_CreateLeadTest extends Crm_BaseTest {
 
+	@Test
+	public void createLeadTest() throws InterruptedException {
+		WebDriver driver = getBrowser(); // getBrowser() method comes from Crm_BaseTest,
+		// returning the current thread’s WebDriver instance — so you don’t need to
+		// initialize it here.
+		CRM_LeadsPage lp = hp.clickLeads();// navigates from hme page and clicks on the click leads method and driver is
+											// set to the lead page
+		lp.clickCreateLeadButtonAndGetTitle(); // Clicks on the Create Lead button and waits for the title to change
+
+	}
+}
