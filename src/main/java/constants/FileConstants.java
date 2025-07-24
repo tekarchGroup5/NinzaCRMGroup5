a3fa19f066030c2f8f92b900ff9f56fc9d1e3462
Teena Raghav <teenaraghav3017@gmail.com>
2025-07-24 16:16:54 -0500

    Update FileConstants.java

--- src/main/java/constants/FileConstants.java
+++ src/main/java/constants/FileConstants.java
@@ -5,9 +5,8 @@ import utils.CommonUtils;
 public class FileConstants {
 
 	public static String ROOT_PATH = System.getProperty("user.dir");
-	public static final String ADMIN_TEST_DATA_FILE_PATH = ROOT_PATH + "/src/main/java/testData/admin.properties";
-	public static final String COMMON_TEST_DATA_FILE_PATH = ROOT_PATH + "/src/main/java/testData/common.properties";
-	public static final String USER_TEST_DATA_FILE_PATH = ROOT_PATH + "/src/main/java/testData/user.properties";
+		public static final String LOGIN_TEST_DATA_FILE_PATH = ROOT_PATH + "/src/main/java/testData/crm_logintestdata.properties";
+		public static final String LEADTESTDATA_FILE_UPLOAD_PATH = ROOT_PATH+"/src/main/java/testData/LeadTestData.xlsx";
 	public static final String OPPORTUNITY_TEST_DATA_UPLOAD_PATH = ROOT_PATH + "/src/test/resources/testdata/Test Data.xlsx";
 	public static final String TEST_PHOTO_UPLOAD_PATH = ROOT_PATH+"/src/main/resources/css.png";
 	public static final String SCREENSHOTS_FOLDER_PATH = ROOT_PATH+"/src/main/resources/reports/"+CommonUtils.getTimeStamp()+"_NinzaCRM.PNG";
