00286a624dd9635d38ee6c3e1e80947e27e6359e
Sathya H <sathyah@tekarch.com>
2025-07-25 13:34:23 -0700

    updated changes 07/25

--- src/main/java/tests/Crm_CreateUserTest.java
+++ src/main/java/tests/Crm_CreateUserTest.java
@@ -6,8 +6,13 @@ import pages.CRM_CreateUsersPage;
 public class Crm_CreateUserTest extends BaseTest {
 
     @Test
-    public void createUserTest() {
+    public void CUserWithValidFields() {
         CRM_CreateUsersPage createUserPage=hp.navigateToCreateUser();
-        createUserPage.enterUserFullName();
+        createUserPage.enterUserFullName("Test FullName");
+        createUserPage.enterMobileNum("9234567890");
+        createUserPage.enterEmail("tek@arch.com");
+        createUserPage.enterUserName("test123");
+        createUserPage.enterPassword("test123");
+        createUserPage.clickCreateUserButton();
     }
 }
