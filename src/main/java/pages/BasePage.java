727a06e64455b0bf5359cefcf121b6e25818a40b
Priyanka Shinde <priyankashinde@ramkrishnas-MacBook-Pro-2.local>
2025-07-26 08:00:34 -0500

    file added

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -6,16 +6,21 @@ import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 	public WebDriver driver;
+	Select select;
+	JavascriptExecutor js;
+
 	public static Logger logger = LogManager.getLogger("Basepage");
 
 	public BasePage(WebDriver driver) {
@@ -29,29 +34,30 @@ public class BasePage {
 
 	public void handleAlertIfPresent() {
 
-	    try {
-	        int retries = 2; // Retry up to 10 times
-	        for (int i = 0; i < retries; i++) {
-	            try {
-	                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
-	                wait.until(ExpectedConditions.alertIsPresent());
+		try {
+			int retries = 2; // Retry up to 10 times
+			for (int i = 0; i < retries; i++) {
+				try {
+					WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+					wait.until(ExpectedConditions.alertIsPresent());
 
-	                Alert alert = driver.switchTo().alert();
-	                logger.info("Alert found after login with text: " + alert.getText());
+					Alert alert = driver.switchTo().alert();
+					logger.info("Alert found after login with text: " + alert.getText());
 
-	                alert.accept();
-	                logger.info("Alert accepted successfully after login.");
-	                return; // exit method after handling
-	            } catch (TimeoutException | NoAlertPresentException e) {
-	                // No alert yet, wait briefly then retry
-	                Thread.sleep(1000);
-	            }
-	        }
-	        logger.info("No alert appeared after login within retry window.");
-	    } catch (Exception e) {
-	        logger.error("Unexpected error while handling alert: " + e.getMessage());
-	    }
+					alert.accept();
+					logger.info("Alert accepted successfully after login.");
+					return; // exit method after handling
+				} catch (TimeoutException | NoAlertPresentException e) {
+					// No alert yet, wait briefly then retry
+					Thread.sleep(1000);
+				}
+			}
+			logger.info("No alert appeared after login within retry window.");
+		} catch (Exception e) {
+			logger.error("Unexpected error while handling alert: " + e.getMessage());
+		}
 	}
+
 	public void switchToChildWindow() {
 		String parentWindow = driver.getWindowHandle();
 		Set<String> Handles = driver.getWindowHandles();
@@ -77,10 +83,59 @@ public class BasePage {
 		return parentWindow;
 	}
 
-	public static String getTextFromElement(WebElement ele) {//priyanka
+	public static String getTextFromElement(WebElement ele) {// priyanka
 		String data = ele.getText();
 		return data;
 	}
-	
-}
 
+	public void selectText(WebElement ele, String text) {
+		select = new Select(ele);
+		select.selectByVisibleText(text);
+	}
+
+	public void selectValue(WebElement ele, String value) {
+		select = new Select(ele);
+		select.selectByValue(value);
+	}
+
+	public void selectIndex(WebElement ele, int index) {
+		select = new Select(ele);
+		select.selectByIndex(index);
+	}
+
+	public void refreshpage() {
+		driver.navigate().refresh();
+	}
+
+	public void clearElement(WebElement ele) {
+		if (ele.isDisplayed()) {
+			ele.clear();
+		} else {
+			System.out.println(" element is not displayed.");
+		}
+	}
+
+	public void enterText(WebElement ele, String data) {
+		if (ele.isDisplayed()) {
+			clearElement(ele);
+			ele.sendKeys(data);
+			System.out.println("data is entered in the textbox");
+		} else {
+			System.out.println(" element is not displayed.");
+		}
+	}
+
+	public void switchFrame(WebElement frame) {
+		driver.switchTo().frame(frame);
+	}
+
+	public void switchToDefault() {
+		driver.switchTo().defaultContent();
+	}
+
+	public void scrolldown() {
+		js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0, 500)");
+	}
+
+}
