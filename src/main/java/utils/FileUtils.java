a82c3de286dd5358d8a683e9ce28f0c08305fae6
Teena Raghav <teenaraghav3017@gmail.com>
2025-07-27 11:42:50 -0500

    Feature/create opportunity (#10)
    * all the files are updated
    
    * changes in opportunity page and opportunity test
    
    * added utility methods in common utils and some changes in opportunity test

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -8,17 +8,18 @@ import java.util.Properties;
 import constants.FileConstants;
 
 public class FileUtils {
+
 	public static String readLoginPropertiesFile(String key) throws FileNotFoundException, IOException {
 		Properties p = new Properties();
 		p.load(new FileReader(FileConstants.LOGIN_TEST_DATA_FILE_PATH));
 		return p.getProperty(key);
 	}
 
-	
+	public static String readOpportunitiesPropertiesFile(String key) throws FileNotFoundException, IOException {
+		Properties p = new Properties();
+		p.load(new FileReader(FileConstants.OPPORTUNITY_PROPERTIES_FILE_PATH));
+		return p.getProperty(key);
 
-//	public static String readHomePropertiesFile(String key) throws FileNotFoundException, IOException {
-//		Properties p = new Properties();
-//		p.load(new FileReader(FileConstants.HOME_TEST_DATA_FILE_PATH));
-//		return p.getProperty(key);
-//	}
+	}
+
 }
