8791fdecf2ad9359412d2e47a98589f0298c6fe6
TeenaRaghav <teenaraghav3017@gmail.com>
2025-07-30 12:25:46 -0500

    Merge branch 'main' of https://github.com/tekarchGroup5/NinzaCRMGroup5.git into main

--- src/main/java/tests/BaseTest.java
+++ src/main/java/tests/BaseTest.java
@@ -114,7 +114,7 @@ public class BaseTest {
 		test.set(extent.createTest(method.getName()));
 
 		// Initialize and configure driver
-		setDriver(browserName, false);
+		setDriver(browserName, true);
 		WebDriver driver = getBrowser();
 		driver.manage().window().maximize();//added this code later to maximize the browser window, this will be a git conflict
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
